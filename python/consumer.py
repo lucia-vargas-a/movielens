aff021455640f80e007324e3018881e9a8faa227
Lucia <lucia.vargas@marleyspoon.com>
2018-10-25 13:40:03 -0500

    working okay

--- python/consumer.py
+++ python/consumer.py
@@ -18,14 +18,6 @@ graph = Graph(url)
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 
-# dataset files
-movie_path = "'file:///%s'" % self.config['files']['movie']
-rating_path = "'file:///%s'" % self.config['files']['rating']
-tag_path = "'file:///%s'" % self.config['files']['tag']
-links_path = "'file:///%s'" % self.config['files']['links']
-genome_tags_path = "'file:///%s'" % self.config['files']['genome_tags']
-genome_scores_path = "'file:///%s'" % self.config['files']['genome_scores']
-
 def get_config():
     """
     Function to obtain the main parameters of source dataset and connections to the database from a configuration file
@@ -45,10 +37,17 @@ class Moviliens_Consumer(object):
     def __init__(self, config_file):
         self.config = config_file
 
+        # read the dataset file names form the configuration file
+        self.movies_path = "'file:///%s'" % self.config['files']['movie']
+        self.rating_path = "'file:///%s'" % self.config['files']['rating']
+        self.tag_path = "'file:///%s'" % self.config['files']['tag']
+        self.links_path = "'file:///%s'" % self.config['files']['links']
+        self.genome_tags_path = "'file:///%s'" % self.config['files']['genome_tags']
+        self.genome_scores_path = "'file:///%s'" % self.config['files']['genome_scores']
 
     def create_constraints(self):
         try:
-            # create constraints for unique propoerties
+            # Create constraints for unique propoerties
             query_unique = """CREATE CONSTRAINT ON (u:User) ASSERT u.user_id IS UNIQUE;"""
             graph.run(query_unique)
             query_unique = """CREATE CONSTRAINT ON (m:Movie) ASSERT m.movie_id IS UNIQUE;"""
@@ -60,48 +59,52 @@ class Moviliens_Consumer(object):
             query_unique = """CREATE CONSTRAINT ON (d:Database) ASSERT d.database_id IS UNIQUE;"""
             graph.run(query_unique)
         except ValueError:
+            logger.exception("message")
             return None
 
-    def create_nodes(self):
+    def create_from_dataset(self):
         """
-            Function to read the dataset and create the nodes in the database
+            Function to read the full dataset and create the nodes, relations and attributes in the database
         """
         try:
             # Load Movie nodes
             query = """
             USING PERIODIC COMMIT
             LOAD CSV WITH HEADERS FROM %s AS mline
-            WITH mline LIMIT 10
-            MERGE (movie:Movie {movie_id: ToInt(mline.movieId)})
+            WITH mline
+            MERGE (movie:Movie {movie_id:ToInt(mline.movieId)})
             ON CREATE SET movie.title = mline.title
             WITH split(mline.genres, "|") AS genre
             UNWIND genre as g
             MERGE (gen:Genre {genre_name:g})
-            MERGE (movie)-[:IN_GENRE]->(gen);
-            ;""" % movie_path
+            MERGE (movie)-[:HAS_GENRE]->(gen)
+            ;""" % self.movies_path
+            logger.info("Loading movie nodes with query %s")
             graph.run(query)
 
-            #load movie databases IDs
+            #Load movie databases IDs
             query = """
             USING PERIODIC COMMIT
             LOAD CSV WITH HEADERS
             FROM %s AS line
-            WITH line LIMIT 10
+            WITH line
             MATCH (movie:Movie {movie_id: line.movieId})
             SET movie.imdb_id = line.imdbId,
-                movie.tmdb_id = line.tmdbId;
-            ;""" % links_path
+                movie.tmdb_id = line.tmdbId
+            ;""" % self.links_path
             graph.run(query)
 
-            #load genome tags
+            #Load movie tags
             query = """
             USING PERIODIC COMMIT
             LOAD CSV WITH HEADERS
-            FROM %s AS tline
-            WITH tline LIMIT 10
-            MERGE(tag:Tag {tag_id: tline.tagId})
-            SET tag.tag_id = tline.tag;
-            ;""" % genome_tags_path
+            FROM %s AS xline
+            MATCH (movie:Movie {movie_id:xline.movieId})
+            WITH xline
+            MERGE (tag:Tag{tag_id:xline.tagId})
+            MERGE (movie)-[h:HAS_TAG]->(tag)
+            SET h.relevance = xline.relevance
+            ;""" % self.genome_scores_path
             graph.run(query)
 
             # Load ratings
@@ -109,55 +112,27 @@ class Moviliens_Consumer(object):
             LOAD CSV WITH HEADERS
             FROM %s as rline
             MERGE (user:User {userId:rline.userId})
-            WITH rline LIMIT 5
+            WITH rline
             MATCH (movie:Movie {movieId:rline.movieId})
             CREATE (user)-[r:RATED]->(movie)
             SET r.rating = toFloat(rline.rating),
-                r.timestamp = ToInt(rline.timestamp);
-            """ % rating_path
+                r.timestamp = ToInt(rline.timestamp)
+            ;""" % self.rating_path
 
             # Load movie-user tags
             query = """
             USING PERIODIC COMMIT
             LOAD CSV WITH HEADERS
             FROM %s AS tline
-            MATCH (user:User{user_id:tline.userId})
+            MERGE (user:User{user_id:tline.userId})
+            WITH tline
             MATCH (movie:Movie{movie_id:tline.movieId})
             CREATE (user)-[a:TAGGED]->(movie)
-            SET a.timestamp = tline.timestamp;
-            ;""" % tag_path
-            graph.run(query)
-
-            # Load Tag nodes
-            query = """
-            USING PERIODIC COMMIT
-            LOAD CSV WITH HEADERS
-            FROM %s AS mline
-            WITH SPLIT(mline.genre,'|') as genre
-            MERGE (genre:Genre {genre_name:genre})
-            ;""" % movie_path
+            SET a.timestamp = tline.timestamp,
+                a.tag = tline.tag
+            ;""" % self.tag_path
             graph.run(query)
 
         except ValueError:
+            logger.exception("message")
             return None
-
-
-    def create_relations(self):
-
-        # Movie > Tag relation
-        query = """
-        USING PERIODIC COMMIT
-        LOAD CSV WITH HEADERS
-        FROM %s AS rline
-        CREATE (user)-[HAS]->(genre)
-        ;""" % rating_path
-        graph.run(query)
-
-        # User > Rating relation
-        query = """
-        USING PERIODIC COMMIT
-        LOAD CSV WITH HEADERS
-        FROM %s AS rline
-        CREATE (user)-[r:RATED]->(movie)
-        SET r.rating = rline.rating, r.timestamp = TOINT(rline.genre)
-        ;""" % rating_path
--- python/main.py
+++ python/main.py
@@ -4,18 +4,28 @@ This main function manages the pipeline to cosume and handle data in neo4j
 
 from python.consumer import Moviliens_Consumer, get_config
 import logging
+import time
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
 logger = logging.getLogger()
 
 
 if __name__ == '__main__':
+    start = time.strftime("%Y-%m-%d %H:%M:%s", time.localtime(time.time()))
+    logger.info('Process start at %s', str(start))
 
     # Get the configuration parameters from the file
     config_file = get_config()
 
     # Instance to consume data
     consumer = Moviliens_Consumer(config_file)
+
+    # Create the indexes required before loading data into neo4j
+    constraints_start = time.time()
     consumer.create_constraints()
-    consumer.create_nodes()
-    consumer.create_relations()
+    logger.info("Constraints created.\nThe process took {0} seconds\n\n".format(time.time() - constraints_start))
+
+    # Load dataset into neo4j database: nodes, relations and attributes
+    consume_start = time.time()
+    consumer.create_from_dataset()
+    logger.info("Constraints created.\nThe process took {0} seconds\n\n".format(time.time() - consume_start))
